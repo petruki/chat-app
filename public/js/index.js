04633facdd12d14d50ae922333e1c5e5fd6f6e7c
petruki <31597636+petruki@users.noreply.github.com>
2020-06-08 21:18:56 -0700

    fix: Room selection now works

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 const socket = io()
-var createOrJoin = true
+var createOrJoin = false
 
 // Elements
 const $rooms = document.querySelector('#rooms')
 const $createOrJoin = document.querySelector('#createOrJoin')
 const $roomSelecionDiv = document.querySelector('#roomSelecionDiv')
 const $inputRoomName = document.querySelector('#inputRoomName')
+const $labelJoin = document.querySelector('#labelJoin')
+const $labelCreate = document.querySelector('#labelCreate')
 
 socket.emit('getRooms', (rooms) => {
     if (!rooms.length) {
@@ -13,6 +15,12 @@ socket.emit('getRooms', (rooms) => {
         $roomSelecionDiv.style.display = "none"
     } else {
         $rooms.style.display = !createOrJoin ? "block" : "none"
+        $inputRoomName.style.display = "none"
+        $inputRoomName.name = ""
+        $inputRoomName.toggleAttribute("required")
+
+        $labelJoin.style.color = 'blueviolet'
+        $labelCreate.style.color = 'none'
     }
 
     for (var i = 0; i < rooms.length; i++) {
@@ -32,12 +40,18 @@ $createOrJoin.addEventListener('click', () => {
 
 const toggleCreateJoin = () => {
     if (createOrJoin) {
+        $labelJoin.style.color = 'blueviolet'
+        $labelCreate.style.color = '#777'
+
         $inputRoomName.style.display = "none"
         $inputRoomName.name = ""
 
         $rooms.style.display = "block"
         $rooms.name = "room"
     } else {
+        $labelJoin.style.color = '#777'
+        $labelCreate.style.color = 'blueviolet'
+
         $inputRoomName.style.display = "block"
         $inputRoomName.name = "room"
 
